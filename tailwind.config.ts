dcd0f0eec1e465b10f10011b3679e7a7791d13d0
offlode <chaimfischer2@gmail.com>
2025-10-31 01:41:08 -0400

    Format Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,1 +1,14 @@
-export default { content:['./src/**/*.{ts,tsx}'], theme:{extend:{colors:{primary:'#6E9FC7',cream:'#F8F4EF',green:'#96CBA8',navy:'#1C2A39',graylight:'#F2F2F2'}}}}
+export default {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6E9FC7",
+        cream: "#F8F4EF",
+        green: "#96CBA8",
+        navy: "#1C2A39",
+        graylight: "#F2F2F2",
+      },
+    },
+  },
+};
